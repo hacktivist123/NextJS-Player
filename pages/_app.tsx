e1efc859ddd74a4ee49d5da7eb3ba451094fdfd4
Shedrack akintayo <akintayoshedrack@gmail.com>
2022-06-13 20:45:46 +0100

    fix import error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import PlayerLayout from "./components/PlayerLayout";
+import PlayerLayout from "./components/PlayerLayout.tsx";
 import "reset-css";
 import "../styles/globals.css";
 
